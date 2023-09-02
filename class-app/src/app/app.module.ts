31d040dd32ba70a4ab59e2f11bb5836433524f95
Alayshia Knighten <alayshia@Alayshias-MacBook-Pro.local>
2023-09-01 21:42:47 -0400

    completed week 2 assignment
    Signed-off-by: Alayshia Knighten <alayshia@Alayshias-MacBook-Pro.local>

--- class-app/src/app/app.module.ts
+++ class-app/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PizzaAppComponent } from './pizza-app/pizza-app.component';
+import { SizePipe } from './shared/pipes/size.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, PizzaAppComponent, SizePipe],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
